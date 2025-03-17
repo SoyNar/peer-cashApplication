59d1f0348b2b9714c91dc2ddb8aef7e64d55cc9a
SoyNar <narciris.tech@gmail.com>
2025-03-17 12:02:17 -0500

    add changes

---
+++ src/main/java/com/peercash/PeerCashproject/PeerCashprojectApplication.java
@@ -0,0 +1,13 @@
+package com.peercash.PeerCashproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerCashprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerCashprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peercash/PeerCashproject/PeerCashprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peercash.PeerCashproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerCashprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
